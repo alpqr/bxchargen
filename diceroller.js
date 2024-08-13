85d1fbfa4904083d59a97f7bbfb282dc0203bba1
Laszlo Agocs <lagocs83@gmail.com>
2024-08-13 19:57:48 +0200

    Add d66 and d666 to diceroller

--- diceroller.js
+++ diceroller.js
@@ -24,26 +24,42 @@ for ( ; ; ) {
             { name: "8) d20", value: 20 },
             { name: "9) d100", value: 100 },
             new Separator(),
+            { name: "d66", value: 66 },
+            { name: "d666", value: 666 },
+            new Separator(),
             { name: "Q) Quit", value: 0 }
         ],
-        pageSize: 12
+        pageSize: 14
     });
     if (sides === 0)
         break;
 
-    const count = Number(await input({
-        message: "How many",
-        default: "1",
-        validate: (value) => { const n = Number(value); return !isNaN(n) && n >= 1 && n <= 20; }
-    }));
+    var count = 1, bonus = 0, result = 0;
 
-    const bonus = Number(await input({
-        message: "Add",
-        default: "0",
-        validate: (value) => { const n = Number(value); return !isNaN(n) && n >= -100 && n <= 100; }
-    }));
+    if (sides == 66) {
+        const a = roll(6, 1, 0);
+        const b = roll(6, 1, 0);
+        result = a * 10 + b;
+    } else if (sides == 666) {
+        const a = roll(6, 1, 0);
+        const b = roll(6, 1, 0);
+        const c = roll(6, 1, 0);
+        result = a * 100 + b * 10 + c;
+    } else {
+        count = Number(await input({
+            message: "How many",
+            default: "1",
+            validate: (value) => { const n = Number(value); return !isNaN(n) && n >= 1 && n <= 20; }
+        }));
 
-    const result = roll(sides, count, bonus);
+        bonus = Number(await input({
+            message: "Add",
+            default: "0",
+            validate: (value) => { const n = Number(value); return !isNaN(n) && n >= -100 && n <= 100; }
+        }));
+
+        result = roll(sides, count, bonus);
+    }
 
     console.log(`\n${count}d${sides}${bonus > 0 ? '+' + bonus : bonus < 0 ? bonus : ''} -> `
         + chalk.bold(chalk.magentaBright(`${result}\n`)));
